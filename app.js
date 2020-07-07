09ab598624e0583fabe44762a2a2a3c67d3e8e93
TheResinger <morrowbenjamin42@gmail.com>
2020-07-07 14:58:15 -0400

    Added models for user and todos and code for saving/testing user being added to database

--- app.js
+++ app.js
@@ -14,4 +14,18 @@ const dbConnectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017
 
 mongoose.connect(dbConnectionString, {useNewUrlParser : true, useUnifiedTopology: true}).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
 
+// Test code for new user being added
+// const User = require('./models/User');
+// const userInput = {
+//     username : "TestGuy123",
+//     password : "1234567",
+//     role : "admin"
+// }
+
+// const user = new User(userInput);
+// user.save((err, document) => {
+//     if(err) console.log(err);
+//     console.log(document);
+// })
+
 app.listen(port, () => { console.log(`Server is listening at http://localhost:${port}`)});
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+const UserSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true
+    },
+    password : {
+        type: String,
+        required : true
+    },
+    role : {
+        type : String,
+        enum : ["admin", "user"],
+        required : true
+    },
+    todos : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Todo' }],
+});
+
+UserSchema.pre('save', function(next) {
+    if(!this.isModified('password')) return next();
+
+    bcrypt.hash(this.password, 10, (err, hashedPass) => {
+        if(err) return next(err);
+        this.password = hashedPass;
+        next();
+    })
+});
+
+UserSchema.methods.comparePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if(err) return callback(err);
+        else{
+            if(!isMatch) return callback(null, isMatch);
+            return callback(null, this);
+        }
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
