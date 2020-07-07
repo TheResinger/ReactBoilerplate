3cfc4c170d23a968173c02e6c53f6d447ba4c0b1
TheResinger <morrowbenjamin42@gmail.com>
2020-07-07 14:27:07 -0400

    Initial setup, created react app, and set up main app.js file

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cookieParser());
+app.use(express.json());
+
+// Change connection string to PROCESS.ENV.
+const dbConnectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernauth';
+
+mongoose.connect(dbConnectionString, {useNewUrlParser : true, useUnifiedTopology: true}).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
+
+app.listen(port, () => { console.log(`Server is listening at http://localhost:${port}`)});
