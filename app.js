d7b51470cac366772a728980db72b9f16d9360dd
TheResinger <morrowbenjamin42@gmail.com>
2020-07-07 22:07:24 -0400

    added heroku fix to server the build react app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,11 +9,27 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Change connection string to PROCESS.ENV.
-const dbConnectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernauth';
+const dbConnectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mernauth';
 
-mongoose.connect(dbConnectionString, {useNewUrlParser : true, useUnifiedTopology: true}).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
+mongoose
+  .connect(dbConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const userRouter = require('./routes/User');
 app.use('/user', userRouter);
 
-app.listen(port, () => { console.log(`Server is listening at http://localhost:${port}`)});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
