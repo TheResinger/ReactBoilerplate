8287d8642ff9a7b9c6484faa505a60e9d3c99d49
TheResinger <morrowbenjamin42@gmail.com>
2020-07-07 22:57:46 -0400

    Updates

---
+++ precache-manifest.c9ac11f134e52a385330fe1e2fde2613.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61b4814207317401d372f0b34200af8",
+    "url": "/ReactBoilerplate/index.html"
+  },
+  {
+    "revision": "645589d1f054b04e2af8",
+    "url": "/ReactBoilerplate/static/js/2.624f1061.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactBoilerplate/static/js/2.624f1061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5e6016f174a5127ec9",
+    "url": "/ReactBoilerplate/static/js/main.dd729604.chunk.js"
+  },
+  {
+    "revision": "f4e0630ecb1b43b24f7a",
+    "url": "/ReactBoilerplate/static/js/runtime-main.2d6c455a.js"
+  }
+]);
